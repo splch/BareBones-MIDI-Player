a368d12be17f4877a1cbaac7d762c265fa85529a
spencerchurchill <spencer.churchill@outlook.com>
2019-02-05 08:43:59 +0000

    updated index name and resizing

--- background.js
+++ background.js
@@ -1,8 +1,9 @@
 chrome.app.runtime.onLaunched.addListener(function() {
-    chrome.app.window.create('window.html', {
+    chrome.app.window.create('index.html', {
         'outerBounds': {
             'width': 200,
             'height': 270
-        }
+        },
+        "resizable": false,
     });
 });
