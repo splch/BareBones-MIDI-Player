1d44990640360e41007accb65d5cce068920449d
spencerchurchill <spencer.churchill@outlook.com>
2019-02-05 08:20:40 +0000

    added dancing skeleton feature and cleaned up button functionality

--- background.js
+++ background.js
@@ -1,7 +1,7 @@
 chrome.app.runtime.onLaunched.addListener(function() {
     chrome.app.window.create('window.html', {
         'outerBounds': {
-            'width': 270,
+            'width': 200,
             'height': 270
         }
     });
--- scripts/script.js
+++ scripts/script.js
@@ -1,21 +1,49 @@
-var midi;
-var sss = 0;
-document.getElementById("load").onchange = function() {
+var midi = null;
+var isPlaying = false;
+
+function flip() {
+    this.isFlipped;
+    if (this.isFlipped === true) {
+        document.getElementsByTagName("img")[0].removeAttribute("style");
+        this.isFlipped = false;
+    }
+    else {
+        document.getElementsByTagName("img")[0].setAttribute("style", "transform: scaleX(-1)");
+        this.isFlipped = true;
+    }
+}
+function trydance() {
+    if (isPlaying === false) {
+        return;
+    }
+    flip();
+    setTimeout(trydance, 700);
+}
+document.getElementById("load").onclick = function() {
+    document.getElementById("filein").click();
+};
+document.getElementById("filein").onchange = function() {
     var reader = new FileReader();
-    if (document.getElementById("load").files[0]) {
-        reader.readAsDataURL(document.getElementById("load").files[0]);
+    if (document.getElementById("filein").files[0]) {
+        reader.readAsDataURL(document.getElementById("filein").files[0]);
         reader.onload = function(e) {
             midi = reader.result;
+            document.getElementsByTagName("small")[0].innerHTML = document.getElementById("filein").files[0].name;
         };
     }
 };
 document.getElementById("play").onclick = function() {
     if (midi) {
         MIDIjs.play(midi);
-        document.getElementsByTagName("small")[0].innerHTML = "playing...";
+        document.getElementById("play").setAttribute("disabled", true);
+        document.getElementById("stop").removeAttribute("disabled", true);
+        isPlaying = true;
+        setTimeout(trydance, 700);
     }
 };
 document.getElementById("stop").onclick = function() {
     MIDIjs.stop();
-    document.getElementsByTagName("small")[0].innerHTML = "";
+    document.getElementById("stop").setAttribute("disabled", true);
+    document.getElementById("play").removeAttribute("disabled", true);
+    isPlaying = false;
 };
