a9ac69cdee821e53abe8f43462a5c818cccd6100
spencerchurchill <spencer.churchill@outlook.com>
2019-03-22 22:35:12 +0000

    updated MIDIjs libraries and some minor improvements http://midijs.net

--- scripts/script.js
+++ scripts/script.js
@@ -1,4 +1,4 @@
-var midi, prog, dancing;
+var prog, dancing;
 function flip() {
     this.isFlipped;
     if (this.isFlipped === false) {
@@ -12,6 +12,11 @@ function flip() {
 function progress() {
     document.getElementsByTagName("progress")[0].value++;
 }
+function clear() {
+    clearInterval(prog);
+    clearInterval(dancing);
+    dancing = prog = null;
+}
 document.getElementById("load").onclick = function() {
     document.getElementById("filein").value = null;
     document.getElementById("filein").click();
@@ -19,29 +24,25 @@ document.getElementById("load").onclick = function() {
 document.getElementById("filein").onchange = function() {
     var reader = new FileReader();
     reader.readAsDataURL(document.getElementById("filein").files[0]);
-    clearInterval(prog);
-    clearInterval(dancing);
-    dancing = prog = null;
+    clear();
     reader.onload = function(e) {
-        midi = reader.result;
+        MIDIjs.play(reader.result);
+        document.getElementsByTagName("progress")[0].value = 0;
         document.getElementsByTagName("progress")[0].max = Math.round(document.getElementById("filein").files[0].size / 100);
         document.getElementById("control").click();
     };
-    document.getElementById("title").innerHTML = document.getElementById("filein").files[0].name.split('.mid')[0].substring(0, 35);
+    document.getElementById("title").innerHTML = document.getElementById("filein").files[0].name.split(".mid")[0].substring(0, 35);
     document.getElementById("control").disabled = false;
 };
 document.getElementById("control").onclick = function() {
-    document.getElementsByTagName("progress")[0].value = 0;
     if (!dancing) {
-        MIDIjs.play(midi);
-        document.getElementById("control").innerHTML = "&#9724;";
+        MIDIjs.resume();
+        document.getElementById("control").innerHTML = "&#10073;&#10073;";
         prog = setInterval(progress, 1000);
         dancing = setInterval(flip, 700);
         return;
     }
-    MIDIjs.stop();
+    MIDIjs.pause();
     document.getElementById("control").innerHTML = "&#9654;";
-    clearInterval(prog);
-    clearInterval(dancing);
-    dancing = prog = null;
+    clear();
 };
