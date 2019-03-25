d088f159b9dc1ad3887d9f6784de9a73508f84bb
spencerchurchill <spencer.churchill@outlook.com>
2019-03-25 22:03:11 +0000

    minor code improvements

--- scripts/script.js
+++ scripts/script.js
@@ -10,7 +10,12 @@ function flip() {
     this.isFlipped = false;
 }
 function progress() {
-    document.getElementsByTagName("progress")[0].value++;
+    if (document.getElementsByTagName("progress")[0].value < document.getElementsByTagName("progress")[0].max) {
+        document.getElementsByTagName("progress")[0].value++;
+        return;
+    }
+    document.getElementsByTagName("progress")[0].value = 0;
+    document.getElementById("control").click();
 }
 function clear() {
     clearInterval(prog);
@@ -22,13 +27,15 @@ document.getElementById("load").onclick = function() {
     document.getElementById("filein").click();
 };
 document.getElementById("filein").onchange = function() {
-    var reader = new FileReader();
+    let reader = new FileReader();
     reader.readAsDataURL(document.getElementById("filein").files[0]);
     clear();
     reader.onload = function(e) {
         MIDIjs.play(reader.result);
+        MIDIjs.get_duration(reader.result, function(duration) {
+            document.getElementsByTagName("progress")[0].max = duration;
+        });
         document.getElementsByTagName("progress")[0].value = 0;
-        document.getElementsByTagName("progress")[0].max = Math.round(document.getElementById("filein").files[0].size / 100);
         document.getElementById("control").click();
     };
     document.getElementById("title").innerText = document.getElementById("filein").files[0].name.split(".mid")[0];
@@ -46,3 +53,4 @@ document.getElementById("control").onclick = function() {
     document.getElementById("control").innerHTML = "&#9654;";
     clear();
 };
+window.addEventListener("load", MIDIjs.get_duration());
