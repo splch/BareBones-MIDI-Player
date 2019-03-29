c6e2124e36731ab92eb19359d34a575200cdc64e
spencerchurchill <spencer.churchill@outlook.com>
2019-03-29 17:17:25 +0000

    ui updates (unpublished)

--- background.js
+++ background.js
@@ -2,7 +2,7 @@ chrome.app.runtime.onLaunched.addListener(function(launchData) {
     chrome.app.window.create('index.html', {
         'outerBounds': {
             'width': 170,
-            'height': 210
+            'height': 209
         },
         "resizable": false,
     }, function(win) {
