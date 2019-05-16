8c354dd9b515f89cddf137d6b1856c3019372089
spencerchurchill <spencer.churchill@outlook.com>
2019-05-16 16:03:48 -0700

    updated loop

--- background.js
+++ background.js
@@ -2,7 +2,7 @@ chrome.app.runtime.onLaunched.addListener(function(launchData) {
     chrome.app.window.create('index.html', {
         'outerBounds': {
             'width': 168,
-            'height': 219
+            'height': 229
         },
         "resizable": false,
     }, function(win) {
